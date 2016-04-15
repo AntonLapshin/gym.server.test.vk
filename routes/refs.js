2cb5cbf85768da9a5827b00702ab79a8a7951a0a
Anton Lapshin <antvallap@gmail.com>
2016-04-15 11:27:19 +0300

    new version (auto deploy)

--- routes/refs.js
+++ routes/refs.js
@@ -1,1 +1,1 @@
-var Db=require("../db"),$=require("jquery-deferred"),newV=2;module.exports={"default":{handler:function(){return $.Deferred(function(e){e.resolve({success:!0,refs:Db.getRefs()})})}}};
+var Db=require("../db"),$=require("jquery-deferred");module.exports={"default":{handler:function(){return $.Deferred(function(e){e.resolve({success:!0,refs:Db.getRefs()})})}}};
