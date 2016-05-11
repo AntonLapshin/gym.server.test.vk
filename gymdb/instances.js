2aaf77876c67f60a265e454c9cc00644c91eb94d
Anton Lapshin <antvallap@gmail.com>
2016-05-11 07:52:38 -0700

    new version (auto deploy)

---
+++ gymdb/instances.js
@@ -0,0 +1,1 @@
+module.exports={GYMTEST:{host:"ds013250.mlab.com",port:13250,database:"gymtest",username:"gymadmin",password:"24547294"},GYMTESTVK:{host:"ds019960.mlab.com",port:19960,database:"gymtestvk",username:"gymadmin",password:"24547294"},GYMPRODVK:{host:"ds041831.mlab.com",port:41831,database:"gymprodvk",username:"gymadmin",password:"24547294"}};
