a277a681d1ccf16b8cb486bd601b5d7d24bc75de
Anton Lapshin <antvallap@gmail.com>
2016-10-02 08:23:17 +0300

    new version (auto deploy)

--- gymdb/backup_prod.js
+++ gymdb/backup_prod.js
@@ -5,20 +5,19 @@ var fs = require('fs');
 var PlayersCollection = require('./collections/players');
 
 GymDb.init(instances.GYMPRODVK).then(
-    function () {
-      Db.getColl('players').find({$query: {}}).toArray(function(err, data){
-        console.log(data.length);
-        fs.writeFile("players.txt", JSON.stringify(data), function(err) {
-          if(err) {
-            return console.log(err);
-          }
+  function() {
+    Db.getColl('players').find({}).toArray(function(err, data) {
+      console.log(data.length);
+      fs.writeFile("players.txt", JSON.stringify(data), function(err) {
+        if (err) {
+          return console.log(err);
+        }
 
-          console.log("The file was saved!");
-          GymDb.close();
-        }); 
+        console.log("The file was saved!");
+        GymDb.close();
       });
-    },
-    function (err) {
-        console.log(err);
     });
-
+  },
+  function(err) {
+    console.log(err);
+  });
